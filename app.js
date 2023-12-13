b77fce2a0108928dfe31914e79507e660dbf72e4
Tananya24 <60601108+Tananya24@users.noreply.github.com>
2023-12-13 14:21:20 +0700

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 app.use('combine')
 app.get("/",(req,res)=>{
-    req.send("Hello borntoDev");
+    req.send("Hello borntoDev1");
 })
 app.listen(port,()=>{
     debug.log("Listening on port %d"+chalk.green(port))
